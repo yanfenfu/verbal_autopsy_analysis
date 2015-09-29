48efc13d1bc890605376292dda91374d1b94b964
Yanfen <fuyanfen@gmail.com>
2015-09-29 14:28:29 -0700

    source code could do pivot now

--- src/verbal_autospy_functions.py
+++ src/verbal_autospy_functions.py
@@ -3,3 +3,7 @@
 import pandas as pd
 df=pd.read_csv('/Users/Yanfen/Documents/verbal_autopsy_analysis/data/IHME_PHMRC_VA_DATA_ADULT_Y2013M09D11_0.csv',low_memory=False)
 
+def count_per_word(df,word):
+    piv = pd.pivot_table(df, values=word,index=['site'], columns=['gs_text34'],aggfunc=sum)
+    return piv
+
