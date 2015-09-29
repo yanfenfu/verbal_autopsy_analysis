90a1a4223d61410e4da5604ec991431ce1af5b1f
Yanfen <fuyanfen@gmail.com>
2015-09-29 14:00:50 -0700

    data written

--- src/verbal_autospy_functions.py
+++ src/verbal_autospy_functions.py
@@ -1,4 +1,5 @@
 ##load the necessary python modules
 #--------------------
-
+import pandas as pd
+df=pd.read_csv('/Users/Yanfen/Documents/verbal_autopsy_analysis/data/IHME_PHMRC_VA_DATA_ADULT_Y2013M09D11_0.csv',low_memory=False)
 
